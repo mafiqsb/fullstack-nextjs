b4667ba0df2a9de9830eff6f73183c35f1f5efd0
mafiqsb <afiqsam71@gmail.com>
2025-02-25 10:43:39 +0800

    commit 5th

--- app/page.js
+++ app/page.js
@@ -1,40 +1,75 @@
+// 'use client';
+
+// import Link from 'next/link';
+// import Post from './component/post';
+// import styles from './page.module.css';
+// import prisma from '@/lib/prisma';
+
+// async function getPost() {
+//   const getData = await prisma.post.findMany({
+//     where: { published: true },
+//     include: {
+//       author: {
+//         select: { name: true },
+//       },
+//     },
+//   });
+
+//   return getData;
+// }
+
+// export default async function Home() {
+//   const posts = await getPost();
+//   return (
+//     <main className={styles.page}>
+//       <Link href={'/add-post'}>Add</Link>
+
+//       <h1>Feed</h1>
+
+//       {posts.map((post) => {
+//         return (
+//           <Post
+//             key={post.id}
+//             id={post.id}
+//             title={post.title}
+//             content={post.content}
+//             authorName={post.author.name}
+//           />
+//         );
+//       })}
+//     </main>
+//   );
+// }
+
+'use client';
+
 import Link from 'next/link';
 import Post from './component/post';
 import styles from './page.module.css';
-import prisma from '@/lib/prisma';
+import useSWR from 'swr';
 
-async function getPost() {
-  const getData = await prisma.post.findMany({
-    where: { published: true },
-    include: {
-      author: {
-        select: { name: true },
-      },
-    },
-  });
+const fetcher = (url) => fetch(url).then((res) => res.json());
 
-  return getData;
-}
+export default function Home() {
+  const { data: posts, mutate } = useSWR('/api/posts', fetcher);
 
-export default async function Home() {
-  const posts = await getPost();
+  if (!posts) return <p>Loading...</p>;
+
   return (
     <main className={styles.page}>
       <Link href={'/add-post'}>Add</Link>
 
       <h1>Feed</h1>
 
-      {posts.map((post) => {
-        return (
-          <Post
-            key={post.id}
-            id={post.id}
-            title={post.title}
-            content={post.content}
-            authorName={post.author.name}
-          />
-        );
-      })}
+      {posts.map((post) => (
+        <Post
+          key={post.id}
+          id={post.id}
+          title={post.title}
+          content={post.content}
+          authorName={post.author?.name}
+        />
+      ))}
     </main>
   );
 }
---
+++ app/posts.js
@@ -0,0 +1,19 @@
+import prisma from '@/lib/prisma';
+
+export default async function handler(req, res) {
+  if (req.method !== 'GET') {
+    return res.status(405).json({ message: 'Method Not Allowed' });
+  }
+
+  const posts = await prisma.post.findMany({
+    where: { published: true },
+    include: {
+      author: {
+        select: { name: true },
+      },
+    },
+  });
+
+  res.setHeader('Cache-Control', 'no-store, max-age=0'); // Disable caching
+  res.status(200).json(posts);
+}
